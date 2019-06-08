b769018aea69c349cf961814c8195ba3bbdd37bb
Askhat Cheriyazdanov <49688646+askhatCH@users.noreply.github.com>
2019-06-08 15:37:29 +0600

    Add files via upload

---
+++ VK/VKBot.php
@@ -0,0 +1,24 @@
+<?php
+class VKBot
+{
+    public $token = '';
+    public $confirmation_token = '';
+    public function request(string $method, array $options)
+    {
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL => 'https://api.vk.com/method/'.$method.'?'.http_build_query($options),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false
+        ]);
+        curl_exec($ch);
+        curl_close($ch);
+    }
+    public function getID(string $url)
+    {
+        $url_id = trim(substr($url, 15));
+        $get_id = json_decode( file_get_contents('https://api.vk.com/method/utils.resolveScreenName?screen_name='.$url_id.'&access_token='.$this->token.'&v=5.95') );
+        if (is_numeric($get_id->response->object_id)) return $get_id->response->object_id;
+        else return 'Error';
+    }
+}
