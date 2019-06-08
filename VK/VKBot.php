40a5d0746536433e1c1fce6d5c0d50a0669bbeff
Askhat Cheriyazdanov <49688646+askhatCH@users.noreply.github.com>
2019-06-08 19:25:54 +0600

    Comments added

--- VK/VKBot.php
+++ VK/VKBot.php
@@ -14,7 +14,7 @@ class VKBot
         curl_exec($ch);
         curl_close($ch);
     }
-    public function getID(string $url) // Gets user ID
+    public function getID(string $url) // gets user ID
     {
         $url_id = trim(substr($url, 15));
         $get_id = json_decode( file_get_contents('https://api.vk.com/method/utils.resolveScreenName?screen_name='.$url_id.'&access_token='.$this->token.'&v=5.95') );
