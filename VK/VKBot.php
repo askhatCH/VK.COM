2161424f755456dd31ebb909dc90bb7945e7994b
Askhat Cheriyazdanov <49688646+askhatCH@users.noreply.github.com>
2019-06-09 14:11:20 +0600

    Added function «getUserData»

--- VK/VKBot.php
+++ VK/VKBot.php
@@ -21,4 +21,19 @@ class VKBot
         if (is_numeric($get_id->response->object_id)) return $get_id->response->object_id;
         else return 'Error';
     }
+    public function getRegData(int $user_id) // gets registration date of user
+    {
+        if(!is_numeric($user_id)) return 'Error';
+		else{
+		$xml = file_get_contents("https://vk.com/foaf.php?id={$user_id}");
+        preg_match("#<ya:created dc:date=\"(.+?)\"/>#", $xml, $getRegData);
+		$regData = substr(trim($getRegData[1]), 0, -15);
+		$userDataTime = substr(trim($getRegData[1]), 10, -12);
+		$userDataDay = substr(str_replace('-', '.', $getRegData[1]), 8);
+        if ($userDataTime == 'T23' || $userDataTime == 'T22' || $userDataTime == 'T21' ) $userDataDay += 1;
+        $userDataMonth = substr(str_replace('-', '.', $getRegData[1]), 5, -18);
+        $userDataYear = substr(str_replace('-', '.', $getRegData[1]), 0, -21);
+		return $userDataDay.'.'.$userDataMonth.'.'.$userDataYear;
+		}
+    }
 }
